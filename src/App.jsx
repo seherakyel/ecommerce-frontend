57e66080f8b9df5dbbf6ba60044e92cedfdb0a45
Seher Akyel <seherakyl06@gmail.com>
2026-07-28 16:22:42 +0300

    Add login and registration routes to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { Routes, Route, Link } from "react-router-dom";
 import ProductsPage from "./ProductsPage";
 import ProductDetailPage from "./ProductDetailPage";
 import CartPage from "./CartPage";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Ürünler</Link> | <Link to="/sepet">Sepetim</Link>
+        <Link to="/">Ürünler</Link> | <Link to="/sepet">Sepetim</Link> |{" "}
+        <Link to="/login">Giriş</Link> | <Link to="/register">Kayıt</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<ProductsPage />} />
         <Route path="/urun/:id" element={<ProductDetailPage />} />
         <Route path="/sepet" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
